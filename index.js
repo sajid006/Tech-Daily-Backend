d16b8901cd65379eb784e0190926171a80f7fa5e
Sajid Hasan <sajid@kelda.no>
2025-02-12 12:45:15 +0600

    Make changes in index file

--- index.js
+++ index.js
@@ -9,6 +9,7 @@ const compression = require('compression');
 const dbConnect = require('./config/dbconnect');
 const cors = require('cors');
 const cookieParser = require('cookie-parser');
+const serverless = require("serverless-http");
 const app = express();
 
 app.use(helmet());
@@ -32,8 +33,8 @@ app.use(express.json());
 app.use(cookieParser());
 dbConnect();
 
-app.use('/api/v1/stories', storyRouter);
-app.use('/api/v1/users', userRouter);
+app.use('/.netlify/functions/app/api/v1/stories', storyRouter);
+app.use('/.netlify/functions/app/api/v1/users', userRouter);
 
 app.all('*', (req, res, next) => {
   next(new AppError(`Cannot find ${req.originalUrl} on this server`, 404));
@@ -41,4 +42,4 @@ app.all('*', (req, res, next) => {
 
 app.use(globalErrorHandler);
 
-module.exports = app;
+module.exports.handler = serverless(app);
